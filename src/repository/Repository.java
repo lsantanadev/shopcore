3fcb60625cbf310ba2a96cf19934b5c9cfae8ab3
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2026-04-28 09:50:16 -0300

    refactor: apply final keyword to product fields and classes for immutability

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 public class Repository<T extends Product> {
-    private HashMap<String, T> products;
+    private final HashMap<String, T> products;
 
     public Repository() {
         this.products = new HashMap<>();
